4357e476f25010ec1286bb1224a286bd30ed22ab
Darya <161479963+polyn47@users.noreply.github.com>
2024-02-28 13:09:53 +0000

    try to commit selector

--- src/Layouts/Page/AboutCompany/AboutCompany.js
+++ src/Layouts/Page/AboutCompany/AboutCompany.js
@@ -3,9 +3,9 @@ import General from "./Components/General/General";
 import Addresses from "./Components/Addresses/Addresses";
 function AboutCompany() {
     return (
-        <div>
+        <div className='d-flex flex-column gap-5 mx-4 px-5 my-5 py-3'>
             <Heading/> {/* Заголовок и кнопка справа */}
-            <div className='d-flex flex-column mt-5'>
+            <div className='d-inline-flex flex-row gap-5'>
                 <General/>  {/* Основная информация вроде названия, типа и телефона */}
                 <Addresses/> {/* Список доступных адресов заведений */}
             </div>
--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -2,7 +2,7 @@ import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
 function Addresses() {
     return (
-        <div className='d-inline-flex flex-column gap-2 mt-4'>
+        <div className='d-inline-flex flex-column gap-2 m-4'>
             <div className='text-rg'>Адреса заведений</div>
             <div className='d-inline-flex flex-column gap-3'>
                 <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
