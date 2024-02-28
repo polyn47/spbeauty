cbe5a63f687692cb234f05a65c2de163e6ad320c
Darya <161479963+polyn47@users.noreply.github.com>
2024-02-28 16:42:50 +0000

    new titledtextinput

--- src/Components/TitledTextInput.js
+++ src/Components/TitledTextInput.js
@@ -2,23 +2,24 @@ import styled from "styled-components";
 
 const Input = styled.input`
     outline: none;
-    border-radius: 0px;
-    border: solid #152842 1px;
-    background-color: #D9E2EF;
-    color: #666787;
+    border-radius: 24px;
+    border: 1px solid var(--Steel-Gray-200, #E2E8F0);
+    background-color: var(--Steel-Gray-50, #F8FAFC);
+    color: var(--Steel-Gray-900, #0F172A);
     &:hover {
-        background-color: #D9E2EFB8;
+        opacity: 0.8;
     }
+    width: 447px;
 `;
 
 function TitledTextInput({title, def}) {
     const Title = title === undefined ? 
         (<div></div>) : 
-        (<div>{title}</div>);
+        (<div className='caption-gray'>{title}</div>);
     return (
         <div className='d-flex flex-column gap-2'>
             {Title}
-            <Input className='p-2' defaultValue={def} ></Input>
+            <Input className='px-3 py-2' defaultValue={def} ></Input>
         </div>
     );
 }
