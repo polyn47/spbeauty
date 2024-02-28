a121aaf3e8046ac41ee2c2fa8d0e4eba0d02968e
Darya <161479963+polyn47@users.noreply.github.com>
2024-02-28 16:29:46 +0000

    new titled selector

--- src/Components/TitledSelector.js
+++ src/Components/TitledSelector.js
@@ -1,18 +1,26 @@
 import styled from "styled-components";
 
 const Select = styled.select`
+    background: url("data:image/svg+xml,<svg height='10px' width='10px' viewBox='0 0 16 16' fill='%23000000' xmlns='http://www.w3.org/2000/svg'><path d='M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z'/></svg>") no-repeat;
+    background-position: calc(100% - 0.75rem) center !important;
+    -moz-appearance:none !important;
+    -webkit-appearance: none !important; 
+    appearance: none !important;
+    outline: none;
     border-radius: 24px;
     border: 1px solid var(--Steel-Gray-200, #E2E8F0);
     background-color: var(--Steel-Gray-50, #F8FAFC);
-    color: #0F172A;
+    color: var(--Steel-Gray-900, #0F172A);
+    width: 447px;
+    }
 `;
 
 function TitledSelector({title, options}) {
-    const Title = title === undefined ? 
-        (<div></div>) : 
-        (<div>{title}</div>);
+    const Title = title === undefined ?
+        (<div></div>):
+        (<div className='caption-gray'>{title}</div>);
     return (
-        <div className='d-inline-flex flex-column gap-2'>
+        <div className='d-flex flex-column gap-2'>
             {Title}
             <Select className='px-3 py-2'>
                 {options.map(optionName => {
--- src/Icons/ArrowDownIcon.js
+++ src/Icons/ArrowDownIcon.js
@@ -1,8 +1,8 @@
 function ArrowDownIcon() {
     return (
-        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M12.6668 6L8.00016 10.6667L3.3335 6" stroke="#94A3B8" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+<path d="M15.8334 7.5L10.0001 13.3333L4.16675 7.5" stroke="#0F172A" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
     );
   }
   
