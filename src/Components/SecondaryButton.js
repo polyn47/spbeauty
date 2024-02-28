27e3c1073c763a2336996f0fbceb3c2216d90c61
Darya <161479963+polyn47@users.noreply.github.com>
2024-02-28 11:13:39 +0000

    new second button

--- src/Components/SecondaryButton.js
+++ src/Components/SecondaryButton.js
@@ -3,18 +3,20 @@ import styled from "styled-components";
 
 function SecondaryButton(props) {
     // Здесь устанавливается минимальная ширина для кнопки
-    let minWidth = props.minWidth === undefined ? '144px' : props.minWidth;
+    let minWidth = props.minWidth === undefined ? '217px' : props.minWidth;
     const Button = styled.div`
-        background: #F2F2F2;
-        border: 1px solid #DFDFDF;
-        box-shadow: inset 0px 1px 2px rgba(10, 15, 26, 0.08);
+        border-radius: 24px;
+        border: 1px solid var(--Steel-Gray-500, #64748B);
+        background: var(--White, #FFF);
         min-width: ${minWidth};
-        color: #152842;
+        color: #64748B;
         &:hover {
-            background: linear-gradient(180deg, #DFEAF1 0%, #CFDDE8 100%);
+            opacity: 0.8;
+            border: 0px solid;
         }
         &:active {
-            background: #DFDFDF;
+            opacity: 0.6;
+            border: 0px solid;
         }
     `;
     return (
